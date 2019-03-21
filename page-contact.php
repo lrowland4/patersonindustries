b2b8d4e7f244ec560f54b442cc8cf7b4c644b015
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-03-21 18:27:37 -0400

    Edit changes

--- page-contact.php
+++ page-contact.php
@@ -3,26 +3,25 @@
 <?php get_header(); ?>
 
 <div>
-<!-- HERO IMAGE -->
   <div class="row">
     <div class="twelve columns">
       <div class="hero">
         <?php dynamic_sidebar('hero-contact'); ?>
         <div class="centered"><h1><?php the_title(); ?></h1></div>
       </div>
-     </div>
-  </div>
-
-
- <!-- CONTACT INFO -->
-
- <div class="row">
-    <div class="twelve columns">
-    <?php dynamic_sidebar('contact-text'); ?> 
+      <div class="container">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <?php the_content();
+          }
+        }
+      ?>
+    </div>
     </div>
   </div>
-
-  </div>  
+</div>
 
 <!-- CONTACT INFO -->
 
