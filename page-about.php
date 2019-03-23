f052600e24f428f35765e621ad744c291ba1e731
Jessica-Small <jlsmall@ufl.edu>
2019-03-23 09:42:13 -0400

    update About page with skeleton for all sections

--- page-about.php
+++ page-about.php
@@ -2,7 +2,7 @@
 
 <?php get_header(); ?>
 
-
+<!-- HERO -->
   <div class="row">
     <div class="twelve columns">
       <div class="hero">
@@ -13,8 +13,10 @@
       </div>
     </div>
   </div>
+
+  <!-- COMPANY HISTORY SECTION (WRITTEN IN WP)-->
   <div class="row">
-      <div class="nine columns">
+      <div class="twelve columns">
         <?php
             if(have_posts()){
               while(have_posts()){
@@ -24,15 +26,67 @@
             }
         ?>
       </div>
-      <div class="nine columns">
-        <?php dynamic_sidebar('about-page'); ?>
-      </div>
-      <div class="twelve columns">
-        <?php dynamic_sidebar('cta-buttons-about'); ?>
-      </div>
+  </div>
+
+  <!-- SAFETY SECTION -->
+  <div class="row">
+    <div class="five columns">
+      <!-- image -->
+    </div>
+    <div class="seven columns">
+      <!-- text, LINK TO "SAFETY" CATEGORY OF BLOG -->
     </div>
   </div>
 
+  <!-- MISSION STATEMENT -->
+  <div class="row">
+    <div class="twelve columns">
+      <!-- mission statement -->
+    </div>
+  </div>
+
+  <!-- VALUES -->
+  <div class="row">
+    <div class="three columns">
+      <!-- icon, text -->
+    </div>
+    <div class="three columns">
+      <!-- icon, text -->
+    </div>
+    <div class="three columns">
+      <!-- icon, text -->
+    </div>
+    <div class="three columns">
+      <!-- icon, text -->
+    </div>
+  </div>
+
+  <!-- COMMUNITY SECTION -->
+  <div class="row">
+    <div class="five columns">
+      <!-- image -->
+    </div>
+    <div class="seven columns">
+      <!-- text, LINK TO "COMMUNITY" CATEGORY OF BLOG -->
+    </div>
+  </div>
+
+  <!-- BIOS SECTION -->
+  <div class="row">
+    <div class="twelve columns">
+      <!-- image -->
+    </div>
+  </div>
+
+  <!-- JOIN OUR TEAM SECTION -->
+  <div class="row">
+    <div class="twelve columns">
+      <?php dynamic_sidebar('cta-buttons-about'); ?>
+    </div>
+  </div>
+
+
+  </div>
 
 <?php get_footer(); ?>
 <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
