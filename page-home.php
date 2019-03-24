9bd6a330e864445b74ac78f047c305399e295d34
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-03-24 17:21:39 -0400

    add scroll arrow

--- page-home.php
+++ page-home.php
@@ -12,6 +12,7 @@
           <h1><?php the_title(); ?></h1>
         </div>
       </div>
+      <a class="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
     </div> 
   </div>
 <!-- ICON SECTION -->
