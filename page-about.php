16df389c232065b53709d9b814456e3f0358f704
Jessica-Small <jlsmall@ufl.edu>
2019-03-24 20:37:58 -0400

    about styling

--- page-about.php
+++ page-about.php
@@ -56,7 +56,7 @@
   </div>
 
   <!-- VALUES -->
-  <div class="row lightpipe divheight">
+  <div class="lightpipe divheight">
     <div class="values">
       <img class="v-icon" src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
     </div>
