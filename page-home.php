ce927bb7072fb9626c12783fe0c991224003116c
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-17 18:26:02 -0400

    styles

--- page-home.php
+++ page-home.php
@@ -72,34 +72,36 @@
 
  <!-- Certifications SECTION -->
  	<div class="certification-background">
- 	<div class="row">
- 		<div class="certification-title">
-          <?php dynamic_sidebar('certification-title-page'); ?>
-        </div>
- 	</div>
-    <div class="row">
-      <div class="four columns">
-        <div class="home-page-1-certification">
-          <?php dynamic_sidebar('certification-page-1'); ?>
-        </div>
-      </div>
-      <div class="four columns">
-        <div class="home-page-2-certification"">
-          <?php dynamic_sidebar('certification-page-2'); ?>
-        </div>
-      </div>
-      <div class="four columns">
-         <div class="home-page-3-certification"">
-          <?php dynamic_sidebar('certification-page-3'); ?>
-        </div>
-      </div>
-      <div class="four columns">
-         <div class="home-page-4-certification"">
-          <?php dynamic_sidebar('certification-page-4'); ?>
-        </div>
-      </div>
+	 	<div class="row">
+	 		<div class="certification-title">
+	          <?php dynamic_sidebar('certification-title-page'); ?>
+	        </div>
+	 	</div>
+
+	    <div class="row">
+	      <div class="four columns">
+	        <div class="home-page-1-certification four columns">
+	          <?php dynamic_sidebar('certification-page-1'); ?>
+	        </div>
+	      </div>
+	      <div class="four columns">
+	        <div class="home-page-2-certification four columns">
+	          <?php dynamic_sidebar('certification-page-2'); ?>
+	        </div>
+	      </div>
+	      <div class="four columns">
+	         <div class="home-page-3-certification four columns">
+	          <?php dynamic_sidebar('certification-page-3'); ?>
+	        </div>
+	      </div>
+	      <div class="four columns">
+	         <div class="home-page-4-certification four columns"">
+	          <?php dynamic_sidebar('certification-page-4'); ?>
+	        </div>
+	      </div>
+	    </div>
     </div>
-    </div>
+
 
 
   </div>
