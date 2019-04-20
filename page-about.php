d73798a1a14b19b55da84df86fb05804e3f5cafc
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-20 01:59:14 -0400

    add animation to values section

--- page-about.php
+++ page-about.php
@@ -50,19 +50,19 @@
   <!-- VALUES -->
   <div class="row lightpipe">
     <div class="values">
-      <img class="v-icon" src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
+      <img class="v-icon  animated wobbleVertical duration1 eds-on-hover " src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
       <p class="center">SAFETY</p>
     </div>
     <div class="values">
-      <img class="v-icon" src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
+      <img class="v-icon  animated wobbleVertical duration1 eds-on-hover " src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
       <p class="center">TRUST</p>
     </div>
     <div class="values">
-      <img class="v-icon" src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
+      <img class="v-icon  animated wobbleVertical duration1 eds-on-hover " src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
       <p class="center">GROWTH</p>
     </div>
     <div class="values">
-      <img class="v-icon" src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
+      <img class="v-icon  animated wobbleVertical duration1 eds-on-hover " src="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/03/services-03.png" alt="value icon">
       <p class="center">TEAMWORK</p>
     </div>
   </div>
