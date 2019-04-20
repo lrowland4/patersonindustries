4884c74fd9f410abbb20a82894a5be58ccb3f46b
Jessica-Small <jlsmall@ufl.edu>
2019-04-19 23:21:19 -0400

    about styling

--- page-about.php
+++ page-about.php
@@ -80,9 +80,18 @@
   </div>
 
   <!-- BIOS SECTION -->
-  <div class="whitebkgrd mobile-c container">
+  <div class="whitebkgrd mobile-c">
     <h3 class="center">Experts you can trust</h3>
     <p class="center"> Our experts are some of the most experienced in the industry. We only hire the best of the best so you can feel reassured that we’re getting the job done right. </p>
+
+      <div class="row">
+        <div class="one-half column">
+          <?php dynamic_sidebar('home-page-2'); ?>
+        </div>
+        <div class="one-half column">
+          <?php dynamic_sidebar('home-page-3'); ?>
+        </div>
+      </div>
   </div>
 
   <!-- JOIN OUR TEAM SECTION -->
