3018c41fc63b67cd68a016eeab51fb6320d6a351
Jessica-Small <jlsmall@ufl.edu>
2019-04-21 15:51:49 -0400

    styling

--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
-<div class="footer">
-  <div class="row container mobile-c">
-    <div class="eight columns">
-      <?php wp_nav_menu(array(
-          'theme_location'    => 'footer-menu',
-          'container-class'   => 'footer'
-        ));
-      ?>
+  <div class="footer">
+    <div class="row container mobile-c">
+      <div class="eight columns">
+        <?php wp_nav_menu(array(
+            'theme_location'    => 'footer-menu',
+            'container-class'   => 'footer'
+          ));
+        ?>
+      </div>
+      <div class="four columns">
+        <?php dynamic_sidebar('bottom-left-contact'); ?>
+      </div>
     </div>
-    <div class="four columns">
-      <?php dynamic_sidebar('bottom-left-contact'); ?>
+    <!-- COPYRIGHT -->
+    <div class="copyright">
+      <p class="small"> &copy; 2019 Paterson Industries Ltd. All Rights Reserved.</p>
     </div>
+    <?php wp_footer(); ?>
   </div>
-  <!-- COPYRIGHT -->
-  <div class="copyright">
-    <p class="small"> &copy; 2019 Paterson Industries Ltd. All Rights Reserved.</p>
-  </div>
-  <?php wp_footer(); ?>
-</div>
 </body>
 </html>
