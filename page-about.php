27899e3ed86af88d0d2d9db7ff577ad02c1e8105
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-21 12:33:54 -0400

    add margn to commutniy

--- page-about.php
+++ page-about.php
@@ -76,7 +76,7 @@
   <div class="row lightgreen-about right-align">
     <div class="seven columns mobile-c pad">
       <h3>Our love for the community</h3>
-      <p>There’s nothing we love more than to show our community how much we care for it. From sponsoring little league baseball teams to volunteering, Paterson Industries Ltd. wants to be involved and make a difference.</p>
+      <p class="about-left">There’s nothing we love more than to show our community how much we care for it. From sponsoring little league baseball teams to volunteering, Paterson Industries Ltd. wants to be involved and make a difference.</p>
       <a href="http://www.jessica-small.com/PatersonIndustries/category/community/" class="button">Learn More</a>
     </div>
     <div class="five columns">
