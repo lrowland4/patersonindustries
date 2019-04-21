bfcbbaccad43eb28e371d10e786885d46ff33e9e
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-21 12:21:09 -0400

    add class to puttons

--- page-about.php
+++ page-about.php
@@ -33,7 +33,7 @@
     <div class="seven columns mobile-c pad">
       <h3>Our commitment to safety</h3>
       <p>We work hard to keep you safe. That’s why we’re certified across board to ensure safety and quality in everything that we do.</p>
-      <a href="http://www.jessica-small.com/PatersonIndustries/category/paterson-industries-ltd/" class="button">Learn More</a>
+      <a class="about-button" href="http://www.jessica-small.com/PatersonIndustries/category/paterson-industries-ltd/" class="button">Learn More</a>
     </div>
   </div>
 
@@ -77,7 +77,7 @@
     <div class="seven columns mobile-c pad">
       <h3>Our love for the community</h3>
       <p>There’s nothing we love more than to show our community how much we care for it. From sponsoring little league baseball teams to volunteering, Paterson Industries Ltd. wants to be involved and make a difference.</p>
-      <a href="http://www.jessica-small.com/PatersonIndustries/category/community/" class="button">Learn More</a>
+      <a class="about-button" href="http://www.jessica-small.com/PatersonIndustries/category/community/" class="button">Learn More</a>
     </div>
     <div class="five columns">
       <?php dynamic_sidebar('about-community'); ?>
