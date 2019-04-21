aae487dca351acd43ecfae87d246b11828a07ad0
Jessica-Small <jlsmall@ufl.edu>
2019-04-21 17:39:41 -0400

    close div

--- page-home.php
+++ page-home.php
@@ -109,23 +109,21 @@
 
 <!-- CTA SECTION -->
 	<div class="lightgreen">
-		<div class="row">
-			 <div class="info-text">
-          <h3>Proudly Serving Canada Since 2014</h3>
-	    </div>
-    </div>
-		<div class="row">
-			<div class="info-image">
-	      <a href="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/04/Glacier-Creative_One-pager-Paterson.pdf" download><?php dynamic_sidebar('info-image'); ?></a>
-	    </div>
-		</div>
-	    <div class="row center cta">
-	      <a class="button cert" href="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/04/Glacier-Creative_One-pager-Paterson.pdf" download>Click to Download Our Company Info Sheet</a>
-	    </div>
-    </div>
-<!-- END CTA SECTION -->
-
+  		<div class="row">
+  			 <div class="info-text">
+            <h3>Proudly Serving Canada Since 2014</h3>
+  	    </div>
+      </div>
+  		<div class="row">
+  			<div class="info-image">
+  	      <a href="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/04/Glacier-Creative_One-pager-Paterson.pdf" download><?php dynamic_sidebar('info-image'); ?></a>
+  	    </div>
+  		</div>
+  	  <div class="row center cta">
+  	    <a class="button cert" href="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/04/Glacier-Creative_One-pager-Paterson.pdf" download>Click to Download Our Company Info Sheet</a>
+  	  </div>
   </div>
+<!-- END CTA SECTION -->
 
 </div>
 
