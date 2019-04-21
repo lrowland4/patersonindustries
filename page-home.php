9ae6f031d62417fbc838003034b65ab270615554
Jessica-Small <jlsmall@ufl.edu>
2019-04-21 16:52:34 -0400

    undo

--- page-home.php
+++ page-home.php
@@ -115,7 +115,7 @@
 	    </div>
 		<div class="row">
 			<div class="info-image">
-	      <a href="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/04/paterson-industries.png"></a>
+	      <a href="http://www.jessica-small.com/PatersonIndustries/wp-content/uploads/2019/04/Glacier-Creative_One-pager-Paterson.pdf" download><?php dynamic_sidebar('info-image'); ?></a>
 	    </div>
 		</div>
 	    <div class="row center cta">
