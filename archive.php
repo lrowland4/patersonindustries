219ced09babeb1d8a9782caa59c1adbcf2940e5b
Jessica-Small <jlsmall@ufl.edu>
2019-04-21 17:56:11 -0400

    close div

--- archive.php
+++ archive.php
@@ -11,37 +11,40 @@
       </div>
   </div>
 
-<div class="container">
-  <div class="row">
-    <div class="twelve columns">
-        <h2>Category: <?php
-            if(is_category()){
-              single_cat_title();
-            }elseif(is_tag()){
-              single_tag_title();
-            }elseif(is_day()){
-              echo "Daily Archives: " . get_the_date();
-            }elseif(is_month()){
-              echo "Monthly Archives: " . get_the_date('F Y');
-            }elseif(is_year()){
-              echo "Yearly Archives: " . get_the_date('Y');
-            }else{
-              echo "Archives";
-            }
-         ?>
-       </h2>
-      <?php if(have_posts()){
-          while(have_posts()){
-              the_post();?>
-              <div class="twelve columns">
-                  <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-                  <?php the_excerpt(); ?>
-              </div>
-              <?php
-            }//ends while loop
-        }//ends if statement
-      ?>
-    </div>
+  <div class="container">
+      <div class="row">
+        <div class="twelve columns">
+              <h2>Category: <?php
+                    if(is_category()){
+                      single_cat_title();
+                    }elseif(is_tag()){
+                      single_tag_title();
+                    }elseif(is_day()){
+                      echo "Daily Archives: " . get_the_date();
+                    }elseif(is_month()){
+                      echo "Monthly Archives: " . get_the_date('F Y');
+                    }elseif(is_year()){
+                      echo "Yearly Archives: " . get_the_date('Y');
+                    }else{
+                      echo "Archives";
+                    }
+                 ?>
+              </h2>
+         </div>
+       </div>
+       <?php if(have_posts()){
+            while(have_posts()){
+                the_post();?>
+                <div class="row">
+                    <div class="twelve columns">
+                        <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+                        <?php the_excerpt(); ?>
+                    </div>
+                </div>
+                <?php
+              }//ends while loop
+          }//ends if statement
+        ?>
   </div>
 </div>
 
