057299cbf55316ab526bf3865036f4674007d5e0
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-21 13:33:04 -0400

    reverse

--- page-about.php
+++ page-about.php
@@ -40,7 +40,7 @@
 <!-- BIOS SECTION -->
   <div class="whitebkgrd mobile-c team">
     <div class="animated fadeInUp duration1 eds-on-scrol">  
-      <h3 class="center">Experts you can trust</h3></div>
+      <h3 class="center">Experts you can trust</h3>
       <p class="center container"> Our experts are some of the most experienced in the industry. We only hire the best of the best so you can feel reassured that we’re getting the job done right. </p>
     </div>
 
